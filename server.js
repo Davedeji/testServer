f628a3ee61f347f9c58ff8e805614838b4c832b6
Deji Toki <adedeji.toki@outlook.com>
2023-11-14 10:02:18 -0800

    up

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const server = http.createServer((req, res) => {
   // if you run the client from sth like file:///C:/nodejs/10httonlyCookie1/index.html
   // it would mean HTML file is being served via the file:// protocol directly from your filesystem rather than over HTTP or HTTPS.
   console.log('Request origin:', req.headers.origin);
+  console.log('Request url:', req.url);
   res.setHeader('Access-Control-Allow-Origin', '*');// Adjust the port if necessary
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
